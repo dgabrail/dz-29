02788213c195ea2c5a3f7bcc9503c544f5c39e8d
Ramazanov Djabrail <ramazanovd612@gmail.com>
2022-11-18 19:16:15 +0600

    the End

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import { stringify } from 'querystring';
+import React, { useState } from 'react';
 import './App.css';
+import Moviess from './components/Movies/Moviess';
+import {MovieName} from './types'
 
 function App() {
+
+  const [Movies , setMovies] = useState<MovieName[]>([])
+
+  const movie  = {
+    name: ''
+  }
+
+  const nameMovie = (event: React.ChangeEvent<HTMLInputElement>) => {
+    
+    movie.name = event.target.value
+  }
+
+  const newMovie = () => {
+    setMovies(prev => [...prev , movie])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={e => nameMovie(e)} style={{margin:'10px'}} type="text"/>
+        <button onClick={newMovie}>Add</button>
+        <Moviess Movies={Movies}/>
     </div>
   );
 }
